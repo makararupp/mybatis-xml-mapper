6e97d12a2ea15e3e3b0f5a05a6b5de4d05519c2f
makaraDevIt <makarasam68@gmail.com>
2026-02-07 10:03:36 +0700

    [update] : clean code

--- src/main/java/co/mcnc/mbmcnc/model/FaqInfo.java
+++ src/main/java/co/mcnc/mbmcnc/model/FaqInfo.java
@@ -8,19 +8,17 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class FaqInfo {
-	 private Long faqId;
-	    private String faqCategoryCode;
-	    private String multilingualContent; // JSON as string
-	    private String attachmentNo;
-	    private String statusCode;
-	    private LocalDateTime systemTimestamp;
-	    private String createdDepart;
-	    private String createdBy;
-	    private String createdDate;
-	    private String createdTime;
-	    private String updatedDepart;
-	    private String updatedBy;
-	    private String updatedDate;
-	    private String updatedTime;
-
+	private Long faqId;
+	private String faqCategoryCode;
+	private String multilingualContent; // JSON as string
+	private String attachmentNo;
+	private String statusCode;
+	private LocalDateTime systemTimestamp;
+	private String createdDepart;
+	private String createdBy;
+	private String createdDate;
+	private String createdTime;
+	private String updatedBy;
+	private String updatedDate;
+	private String updatedTime;
 }
